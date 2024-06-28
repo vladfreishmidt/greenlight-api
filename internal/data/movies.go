5544821b382a07b1877e584fd3f37e3353dc3a58
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-06-28 19:04:56 +0300

    Add ValidateMovie validator

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,6 +3,9 @@ package data
 import (
 	"database/sql"
 	"time"
+
+	"github.com/lib/pq"
+	"github.com/vladfreishmidt/greenlight-api/internal/validator"
 )
 
 type Movie struct {
@@ -15,12 +18,36 @@ type Movie struct {
 	Version   int32     `json:"version"`
 }
 
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
 type MovieModel struct {
 	DB *sql.DB
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	query := `
+	INSERT INTO movies (title, year, runtime, genres) 
+	VALUES ($1, $2, $3, $4)
+	RETURNING id, created_at, version`
+
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
